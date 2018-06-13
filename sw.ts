e3999ab37719e8a5b2ba7f41e10c6929ba336fc3
hrk fkm <fkm@mokelab.com>
2018-06-14 01:36:09 +0900

    add index.html to cache

--- sw.ts
+++ sw.ts
@@ -4,6 +4,10 @@ importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.2.0/workbox
 
 if (workbox) {
     workbox.routing.registerRoute(
+        'https://fkmhrk.github.io/pokemongo-stardust-and-candy/index.html',
+        workbox.strategies.networkFirst()
+    );
+    workbox.routing.registerRoute(
         new RegExp('.*\.js'),
         workbox.strategies.networkFirst()
     );
